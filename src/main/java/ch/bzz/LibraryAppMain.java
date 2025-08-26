a48f2025159d1b26aab47e7ccc6031fe6a9292c2
Mkw68Mkw <mkw68mkw@outlook.de>
2025-08-26 09:30:30 +0200

    anforderung1&anforderung2

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,8 +1,41 @@
 package ch.bzz;
+import java.util.Scanner;
+
 
 public class LibraryAppMain {
+    private static final Book BOOK_1 = new Book(1, "978-3-8362-9544-4", "Java ist auch eine Insel", "Christian Ullenboom", 2023);
+    private static final Book BOOK_2 = new Book(2, "978-3-658-43573-8", "Grundkurs Java", "Dietmar Abts", 2024);
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("LibraryApp gestartet. Tippe 'help' für Befehle.");
+
+        boolean running = true;
+        while (running) {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+
+            switch (input) {
+                case "quit":
+                    running = false;
+                    break;
+                case "help":
+                    System.out.println("Verfügbare Befehle: quit, help, listBooks");
+                    break;
+                case "listBooks":
+                    listBooks();
+                    break;
+                default:
+                    System.out.println("Unbekannter Befehl: " + input);
+                    break;
+            }
+        }
+        scanner.close();
+    }
+
+    private static void listBooks() {
+        System.out.println(BOOK_1.getTitle());
+        System.out.println(BOOK_2.getTitle());
     }
 }
